4176850a41a54269f2917c7495ea829ecd6ad3fd
Maaz <164782753+maaz-i7@users.noreply.github.com>
2025-11-16 23:50:32 +0530

    Added password hashing

--- Secure_Authentication/Person.java
+++ Secure_Authentication/Person.java
@@ -1,32 +1,87 @@
+import java.util.Arrays;
+
 abstract class Person {
 
-    /*
-     * name        - full name of the person
-     * username    - unique username for each person
-     * accountType - the type of employee the person is, viz, {"Admin", "Agronomist", "Farmer"}
-     * password    - password to login to account
-     * phoneNum    - phoneNumber of the person
-     * salary      - salary of the person
+    /*      
+     * name              - full name of the person
+     * username          - unique username for each person
+     * accountType       - the type of person, viz, {"Admin", "Agronomist", "Farmer"}
+     * hashedPassword[]  - an array of numbers with password hashed in it
+     * phoneNum          - phoneNumber of the person
+     * salary            - salary of the person
      */
     public String name;
     public String username;
     public String accountType;
-    private String password;
+    private int[] hashedPassword;
     private long phoneNum;
     private double salary;
-
+    
     public Person(String name, String username, String accountType, String password, long phoneNum, double salary) {
 
         this.name = name;
         this.username = username;
         this.accountType = accountType;
-        this.password = password;
+        this.hashedPassword = hashPassword(password);
         this.phoneNum = phoneNum;
         this.salary = salary;
     }
+    
+    /*
+     * Simulates the hashing of a password String
+     * First the password is reversed
+     * Then, from 0th index of the reversed password String, the characters' (ASCII values + index_number) is hashed in password[]
+     */
+    private static int[] hashPassword(String password) {
+        
+        int[] hashedPassword = new int[100];
+        Arrays.fill(hashedPassword, -1);
 
+        String reversedPassword = "";
+
+        //password string reversed
+        for(int i = password.length()-1; 0 <= i; i--)
+        reversedPassword += Character.toString(password.charAt(i));
+
+        //(ASCII value of password.charAt(i) + i) is hashed in password[]
+        for(int i = 0; i < reversedPassword.length(); i++)
+        hashedPassword[i] = ((int)reversedPassword.charAt(i)) + i;
+
+        return hashedPassword;
+    }
+    
+    /*
+     * Checks if the user entered a correct password to login
+     * dehashes the password and checks it with the enteredPasssword
+     * If password matches, returns true
+     * Otherwise, returns false
+     */
     protected boolean isPasswordMatch(String enteredPassword) {
-        return this.password.equals(enteredPassword);
+
+        int i;
+        int[] hashedPassword = Arrays.copyOf(this.hashedPassword, this.hashedPassword.length);
+
+        for(i = 0; i < 100; i++) {
+            
+            if(hashedPassword[i] == -1)
+            break;
+
+            hashedPassword[i] -= i;
+        }
+
+        i--;
+
+        String password = "";
+
+        for(; 0 <= i; i--) {
+
+            if(hashedPassword[i] == -1)
+            break;
+
+            password += Character.toString((char)(hashedPassword[i]));
+        }
+
+        return password.equals(enteredPassword);
     }
 
     protected long getPhoneNum() {
@@ -38,7 +93,7 @@ abstract class Person {
     }
 
     protected void updatePassword(String newPassword) {
-        this.password = newPassword;
+        this.hashedPassword = hashPassword(newPassword);
     }
 
     protected void updatePhoneNum(long newPhoneNum) {
