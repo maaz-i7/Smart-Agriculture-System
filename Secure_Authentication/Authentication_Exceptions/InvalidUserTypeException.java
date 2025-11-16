b310ecb203f9ca7102ea454ac4c3a802dfe12531
Maaz <164782753+maaz-i7@users.noreply.github.com>
2025-11-16 18:53:55 +0530

    Update InvalidUserTypeException.java

--- Secure_Authentication/Authentication_Exceptions/InvalidUserTypeException.java
+++ Secure_Authentication/Authentication_Exceptions/InvalidUserTypeException.java
@@ -5,6 +5,8 @@ public class InvalidUserTypeException extends Exception {
     public static String conditions = "\n\tInvalid user type";
 
     public InvalidUserTypeException() {
+        
         super(conditions);
     }
 }
+
