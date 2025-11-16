2f0126e2dc2db82f128c33538cea3bf954e27236
Maaz <164782753+maaz-i7@users.noreply.github.com>
2025-11-16 18:50:09 +0530

    Update Admin.java

--- Secure_Authentication/Admin.java
+++ Secure_Authentication/Admin.java
@@ -1,1 +1,7 @@
+public class Admin extends Person {
 
+    public Admin(String name, String username, String accountType, String password, long phoneNum) {
+
+        super(name, username, accountType, password, phoneNum, 100000);
+    }
+}
