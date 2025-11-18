6884c98b0262da183f247946943f04196bd66a55
Maaz <164782753+maaz-i7@users.noreply.github.com>
2025-11-18 10:13:25 +0530

    Add files via upload

---
+++ Smart_Agriculture_System/Secure_Authentication/Encryptor.java
@@ -0,0 +1,63 @@
+package Smart_Agriculture_System.Secure_Authentication;
+import java.util.Arrays;
+import Smart_Agriculture_System.Users.*;
+
+public class Encryptor {
+    
+    /*
+     * Simulates the hashing of a password String
+     * First the password is reversed
+     * Then, from 0th index of the reversed password String, the characters' (ASCII values + index_number) is hashed in password[]
+     */
+    protected static int[] hashPassword(String password) {
+        
+        int[] hashedPassword = new int[100];
+        Arrays.fill(hashedPassword, -1);
+
+        String reversedPassword = "";
+
+        //password string reversed
+        for(int i = password.length()-1; 0 <= i; i--)
+        reversedPassword += Character.toString(password.charAt(i));
+
+        //(ASCII value of password.charAt(i) + i) is hashed in password[]
+        for(int i = 0; i < reversedPassword.length(); i++)
+        hashedPassword[i] = ((int)reversedPassword.charAt(i)) + i;
+
+        return hashedPassword;
+    }
+    
+    /*
+     * Checks if the user entered a correct password to login
+     * dehashes the password and checks it with the enteredPasssword
+     * If password matches, returns true
+     * Otherwise, returns false
+     */
+    protected static boolean isPasswordMatch(Person person, String enteredPassword) {
+
+        int[] hashedPassword = person.getHashedPassword();
+
+        int i;
+        for(i = 0; i < 100; i++) {
+            
+            if(hashedPassword[i] == -1)
+            break;
+
+            hashedPassword[i] -= i;
+        }
+
+        i--;
+
+        String password = "";
+
+        for(; 0 <= i; i--) {
+
+            if(hashedPassword[i] == -1)
+            break;
+
+            password += Character.toString((char)(hashedPassword[i]));
+        }
+
+        return password.equals(enteredPassword);
+    }
+}
