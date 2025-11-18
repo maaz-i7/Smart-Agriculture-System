5ab74ad3368d7d8d6b1f5231750f26f1157ff8c9
Maaz <164782753+maaz-i7@users.noreply.github.com>
2025-11-18 09:56:29 +0530

    Delete Secure_Authentication directory

--- Secure_Authentication/Agronomist.java
+++
@@ -1,7 +0,0 @@
-public class Agronomist extends Person {
-    
-    public Agronomist(String name, String username, String accountType, String password, long phoneNum) {
-
-        super(name, username, accountType, password, phoneNum, 80000);
-    }
-}
--- Secure_Authentication/Farmer.java
+++
@@ -1,7 +0,0 @@
-public class Farmer extends Person {
-    
-    public Farmer(String name, String username, String accountType, String password, long phoneNum) {
-
-        super(name, username, accountType, password, phoneNum, 50000);
-    }
-}
